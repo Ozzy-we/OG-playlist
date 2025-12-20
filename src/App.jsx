2d9187d0d35db1f0ea6d62c2ff07423a3ca12f0f
Ozzy-we <kyawmyo.h66@rsu.ac.th>
2025-12-20 12:58:19 +0700

    NavBar Add

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from './components/Navbar';
+
+function App(){
+  return(
+    <div className="min-h-screen w-full bg-theme-black">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar(){
+    return(
+        <nav className="bg-theme-black px-8 py-6 flex justify-center items-center font-sans">
+            <ul className="flex gap-12 list-none">
+                <li><a href="#tour" className="text-theme-red text-lg font-semibold tracking-wide uppercase">Tour</a></li>
+                <li><a href="#merch" className="text-theme-red text-lg font-semibold tracking-wide uppercase">Merch</a></li>
+                <li><a href="#music" className="text-theme-red text-lg font-semibold tracking-wide uppercase">Music</a></li>
+                <li><a href="#video" className="text-theme-red text-lg font-semibold tracking-wide uppercase">Video</a></li>
+            </ul>
+        </nav>
+    )
+}
+export default Navbar;
